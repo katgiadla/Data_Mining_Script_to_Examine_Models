9a367eeca62db379052e089e33e8d8763a7f0938
katgiadla <katarzyna.giadla@gmail.com>
2019-04-25 21:44:40 +0200

    Rename a source file

---
+++ csvfileoptions.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def read_convert_csv(pathtofile: str):
+    df = pd.read_csv(pathtofile,
+                     names=['DaneRealne', 'Model'],
+                     dtype={'DaneReal': float, 'Model': float})
+    return df
+
+def print_data(mydata: object):
+    print(mydata)
+    return
