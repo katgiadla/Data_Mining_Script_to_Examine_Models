d1d36af0e9a5b6abde7ae869646bb7fa7204f0e5
katgiadla <katarzyna.giadla@gmail.com>
2019-04-25 21:57:48 +0200

    Check existing a file

--- Main.py
+++ Main.py
@@ -2,6 +2,15 @@ import os
 import sys
 from csvfileoptions import read_convert_csv, print_data
 
+def check_file():
+    if (sys.argv).__len__() != 2:
+        return False
+    else:
+        return True
+
 if __name__ == '__main__':
-    df = read_convert_csv('testFile.csv')
+    if (check_file() == False):
+        print("Too much or not much")
+        exit()
+    df = read_convert_csv(sys.argv[1])
     print(df)
