cb2ee9a4e829ff6440013cf0c20fb56f546fa565
katgiadla <katarzyna.giadla@gmail.com>
2019-05-13 23:31:05 +0200

    Correct a language mistake

--- CSVfileoption.py
+++ CSVfileoption.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def read_convert_csv(pathtofile: str):
     df = pd.read_csv(pathtofile,
-                     names=['DaneRealne', 'Model'],
-                     dtype={'DaneReal': float, 'Model': float})
+                     names=['RealData', 'Model'],
+                     dtype={'RealData': float, 'Model': float})
     return df
 
 def print_data(mydata: pd.DataFrame):
--- mapeanalys.py
+++ mapeanalys.py
@@ -4,5 +4,5 @@ import numpy as np
 def calculateError(CSVFile: pd.DataFrame):
     errorTab = np.zeros((len(CSVFile), 1))
     for i in range(0, len(CSVFile)):
-        errorTab[i] = float(CSVFile.iloc[i]['Model'] - CSVFile.iloc[i]['DaneRealne'])
+        errorTab[i] = float(CSVFile.iloc[i]['DaneRealne'] - CSVFile.iloc[i]['Model'])
     return errorTab
