6f162bd2981824aae8978adf8b06345f97e5560d
katgiadla <katarzyna.giadla@gmail.com>
2019-05-13 23:34:08 +0200

    Correct a function logic

--- mapeanalys.py
+++ mapeanalys.py
@@ -4,5 +4,5 @@ import numpy as np
 def calculateError(CSVFile: pd.DataFrame):
     errorTab = np.zeros((len(CSVFile), 1))
     for i in range(0, len(CSVFile)):
-        errorTab[i] = float(CSVFile.iloc[i]['DaneRealne'] - CSVFile.iloc[i]['Model'])
+        errorTab[i] = float(CSVFile.iloc[i]['RealData'] - CSVFile.iloc[i]['Model'])
     return errorTab
