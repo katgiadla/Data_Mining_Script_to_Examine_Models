2f32acb274c48be05f81ae4b276a97afce387ca5
katgiadla <katarzyna.giadla@gmail.com>
2019-05-13 22:38:30 +0200

    Create a function of calculate value of errors

--- Main.py
+++ Main.py
@@ -2,11 +2,12 @@ import sys
 
 from Entryoffile import check_file
 from CSVfileoption import read_convert_csv, print_data
+from mapeanalys import calculateError
 
 if __name__ == '__main__':
     if (check_file() == False):
         print("Too much or not much arguments")
         exit()
     df = read_convert_csv(sys.argv[1])
-    print_data(df.get_values())
-    print_data(len(df))
+    print_data(df)
+    print_data(calculateError(df))
--- mapeanalys.py
+++ mapeanalys.py
@@ -1,4 +1,8 @@
-def calculateError(CSVFile: object):
-    errorTab = []
+import pandas as pd
+import numpy as np
+
+def calculateError(CSVFile: pd.DataFrame):
+    errorTab = np.zeros((len(CSVFile), 1))
     for i in range(0, len(CSVFile)):
-        errorTab.append(CSVFile.get_values())
+        errorTab[i] = float(CSVFile.iloc[i]['Model'] - CSVFile.iloc[i]['DaneRealne'])
+    return errorTab
